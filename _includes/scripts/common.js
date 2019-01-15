18566f7553d5519d8d8b0ce473990d5c705a3a73
zkswe <pengzc@zkswe.com>
2019-01-15 17:15:34 +0800

    fix

--- _includes/scripts/common.js
+++ _includes/scripts/common.js
@@ -4,4 +4,18 @@
     $root.dataset.isTouch = true;
     document.addEventListener('touchstart', function(){}, false);
   }
+
+  var SOURCES = window.TEXT_VARIABLES.sources;
+  window.Lazyload.js(SOURCES.jquery, function() {
+
+    if ("developer.flythings.cn" == document.domain) {
+      $("#hostcompany").show();
+      alert("show footer");
+      try {
+        $("nav").find("ul").append('<li><a href="http://www.zkswe.com">关于我们</a></li>')
+      } catch (e) { 
+      }
+    }
+  });
+
 })();
